cf74f6568e7258428153e33060999d515c18dfd0
MrTrukiny <carloseduardo.tkn@gmail.com>
2021-10-18 11:08:00 -0500

    Implement url verification for Slack Events subscription

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 exports.boosterBot = (event, context, callback) => {
-  const response = { message: 'Hello world!' };
-  callback(null, response);
+  const body = JSON.parse(event.body);
+  switch (body.type) {
+    case 'url_verification':
+      callback(null, body.challenge);
+      break;
+    default:
+      callback(null);
+  }
 };
